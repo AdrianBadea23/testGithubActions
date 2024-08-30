82a99f971a0964e0d290e4512a53c53fdd66b2df
AdrianBadea23 <116947355+AdrianBadea23@users.noreply.github.com>
2024-08-30 11:09:33 +0300

    Delete test1.c

--- test1.c
+++
@@ -1,9 +0,0 @@
-#include <criterion/criterion.h>
-
-Test(sample, addition) {
-    cr_assert_eq(2 + 2, 4, "Expected 2 + 2 to equal 4");
-}
-
-Test(sample, subtraction) {
-    cr_assert_eq(2 - 1, 1, "Expected 2 - 1 to equal 1");
-}
